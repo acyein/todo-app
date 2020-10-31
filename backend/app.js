b7efcfce89ce9dff0fadada565b0f831b106d7de
Yein <chianyein@gmail.com>
2020-10-31 18:51:52 +0800

    Install cors package

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 dotenv.config();
 
 // Import routes
@@ -11,11 +13,12 @@ const port = process.env.PORT || 8000;
 
 // Connect to DB
 mongoose.connect(
-	process.env.MONGODB_URI,
-	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-	() => console.log("Connected to DB")
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => console.log('Connected to DB')
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
